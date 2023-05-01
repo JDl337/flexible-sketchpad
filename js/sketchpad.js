21e5288cfd9e4a2d769f4a9359caaf8cb7a9e7fe
JD <126777940+JDl337@users.noreply.github.com>
2023-05-01 12:55:04 +0530

    implemented user input and limits

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -3,23 +3,36 @@ function hover(boxid) {
     box.classList.add("hovered");
 }
 
-function reset(container) {
+function reset(container, newsize) {
     container.replaceChildren();
+    makegrid(newsize);
 }
+function makegrid(n) {
+    const parent = document.querySelector(".allparent");
 
-const parent = document.querySelector(".allparent");
-
-let n = 80;
-
-for (let i = 0; i < n; i++) {
-    let tmphorz = document.createElement("div");
-    tmphorz.classList.add("rowchild");
-    for (let j = 0; j < n; j++) {
-        let tmpbox = document.createElement("div");
-        tmpbox.classList.add("boxchild");
-        tmpbox.id = `${i}, ${j}`;
-        tmpbox.addEventListener('mouseover', (e) => hover(e.target.id));
-        tmphorz.appendChild(tmpbox);
+    for (let i = 0; i < n; i++) {
+        let tmphorz = document.createElement("div");
+        tmphorz.classList.add("rowchild");
+        for (let j = 0; j < n; j++) {
+            let tmpbox = document.createElement("div");
+            tmpbox.classList.add("boxchild");
+            tmpbox.id = `${i}, ${j}`;
+            tmpbox.addEventListener('mouseover', (e) => hover(e.target.id));
+            tmphorz.appendChild(tmpbox);
+        }
+        parent.appendChild(tmphorz);
     }
-    parent.appendChild(tmphorz);
 }
+
+makegrid(16);
+
+const btn = document.getElementById('reset');
+btn.addEventListener('click', () => {
+    n = prompt("Enter size of new grid: ");
+    const parent = document.querySelector(".allparent");
+    if (n > 100 || n < 0) {
+        alert('Enter a number from 0 to 100');
+        return;
+    }
+    reset(parent, n);
+})
