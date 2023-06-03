b896631a7c47aab1d36cf4599451ea7add6844f0
JD <126777940+JDl337@users.noreply.github.com>
2023-06-03 12:02:24 +0530

    Implement incremental darkening

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,6 +1,14 @@
 function hover(boxid) {
     const box = document.getElementById(boxid);
-    box.classList.add("hovered");
+    let col = window.getComputedStyle(box).backgroundColor;
+
+    col = col.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    let red = parseInt(col[1]);
+    let green = parseInt(col[2]);
+    let blue = parseInt(col[3]);
+
+    box.style.backgroundColor = `rgb(${red-20}, ${green-20}, ${blue-20})`;
+
 }
 
 function reset(container, newsize) {
@@ -18,6 +26,7 @@ function makegrid(n) {
             tmpbox.classList.add("boxchild");
             tmpbox.id = `${i}, ${j}`;
             tmpbox.addEventListener('mouseover', (e) => hover(e.target.id));
+            tmpbox.style.backgroundColor = "white";
             tmphorz.appendChild(tmpbox);
         }
         parent.appendChild(tmphorz);
